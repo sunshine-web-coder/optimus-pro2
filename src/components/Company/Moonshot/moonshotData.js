ae3702b703598a19de65ab1b923167634d6ce32a
sunshine-web-coder <webcoderdev02@gmail.com>
2024-10-13 21:22:54 +0100

    moonshot section updated.

--- src/components/Company/Moonshot/moonshotData.js
+++ src/components/Company/Moonshot/moonshotData.js
@@ -1,17 +1,48 @@
 export const roadmapData = [
   {
-    phase: '2031 & Beyond',
-    title: 'LEADERSHIP',
-    items: [
-      'Establish the Optimus PRO ecosystem as a leading brand in the web3 space',
-      'Drive innovation and development in the decentralized space',
-      'Create a lasting legacy and impact on the future of the internet'
-    ]
+    phase: '2017 - 2020',
+    title: 'Experience Pain Points',
+    items: ['Lost significant funds due to pain points', 'Talked with people with similar experience', 'Conceptualization of solution']
   },
   {
-    phase: '2030',
-    title: 'Global Reach',
-    items: ['Establish a global presence and community', 'Collaborate with governments, institutions, and traditional industries', 'Drive adoption and mainstream acceptance']
+    phase: '2021',
+    title: 'Ideation',
+    items: ['Defined project purpose and goals', 'Brainstorm, research and document solutions', 'Launch a platform to educate people']
+  },
+  {
+    phase: '2022',
+    title: 'Team & Community Building',
+    items: ['Build a small team to kickstart our project', 'Launch our MVC (Minimum Viable Community)', 'Start training and upskilling our team']
+  },
+  {
+    phase: '2023',
+    title: 'Brand Launch',
+    items: ['Launch and incorporate Optimus PRO brand', 'Launch brand awareness campaigns', 'Establish strategic partnerships and collaboration']
+  },
+  {
+    phase: '2024',
+    title: 'Human Infrastructures Launch',
+    items: ['Launch Optimus PRO Academy', 'Launch PROteam', 'Onboard talents to build Optimus PRO']
+  },
+  {
+    phase: '2025',
+    title: 'Human Infrastructures Launch',
+    items: ['Launch Optimus PRO Academy', 'Launch PROteam', 'Onboard talents to build Optimus PRO']
+  },
+  {
+    phase: '2026',
+    title: 'Tech Infrastructures Launch',
+    items: ['Launch Optimus Chain/mainnet', 'Launch Alpha Phase 0', 'Token Launch']
+  },
+  {
+    phase: '2027',
+    title: 'Growth - Establish Other Aims',
+    items: ['Wallet connect', 'Metamask', 'Rug pull protection', 'Path finder screener']
+  },
+  {
+    phase: '2028',
+    title: 'Expansion',
+    items: ['Expand our ecosystem through new use cases and applications', 'Incentivize Developers and Builders to the Optimus PRO ecosystem', 'Develop and implement scaling solutions']
   },
   {
     phase: '2029',
@@ -23,48 +54,17 @@ export const roadmapData = [
     ]
   },
   {
-    phase: '2028',
-    title: 'Expansion',
-    items: ['Expand our ecosystem through new use cases and applications', 'Incentivize Developers and Builders to the Optimus PRO ecosystem', 'Develop and implement scaling solutions']
+    phase: '2030',
+    title: 'Global Reach',
+    items: ['Establish a global presence and community', 'Collaborate with governments, institutions, and traditional industries', 'Drive adoption and mainstream acceptance']
   },
   {
-    phase: '2027',
-    title: 'Growth - Establish Other Aims',
-    items: ['Wallet connect', 'Metamask', 'Rug pull protection', 'Path finder screener']
-  },
-  {
-    phase: '2026',
-    title: 'Tech Infrastructures Launch',
-    items: ['Launch Optimus Chain/mainnet', 'Launch Alpha Phase 0', 'Token Launch']
-  },
-  {
-    phase: '2025',
-    title: 'Human Infrastructures Launch',
-    items: ['Launch Optimus PRO Academy', 'Launch PROteam', 'Onboard talents to build Optimus PRO']
-  },
-  {
-    phase: '2024',
-    title: 'Human Infrastructures Launch',
-    items: ['Launch Optimus PRO Academy', 'Launch PROteam', 'Onboard talents to build Optimus PRO']
-  },
-  {
-    phase: '2023',
-    title: 'Brand Launch',
-    items: ['Launch and incorporate Optimus PRO brand', 'Launch brand awareness campaigns', 'Establish strategic partnerships and collaboration']
-  },
-  {
-    phase: '2022',
-    title: 'Team & Community Building',
-    items: ['Build a small team to kickstart our project', 'Launch our MVC (Minimum Viable Community)', 'Start training and upskilling our team']
-  },
-  {
-    phase: '2021',
-    title: 'Ideation',
-    items: ['Defined project purpose and goals', 'Brainstorm, research and document solutions', 'Launch a platform to educate people']
-  },
-  {
-    phase: '2017 - 2020',
-    title: 'Experience Pain Points',
-    items: ['Lost significant funds due to pain points', 'Talked with people with similar experience', 'Conceptualization of solution']
+    phase: '2031 & Beyond',
+    title: 'LEADERSHIP',
+    items: [
+      'Establish the Optimus PRO ecosystem as a leading brand in the web3 space',
+      'Drive innovation and development in the decentralized space',
+      'Create a lasting legacy and impact on the future of the internet'
+    ]
   }
 ];
